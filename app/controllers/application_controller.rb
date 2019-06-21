716ac845654e9f5ef6a8167d2a221992c1d6a57f
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2019-06-21 07:29:22 -0700

    extract session[:current_email] to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
+# Application controller
 class ApplicationController < ActionController::Base
-  
+
   def authenticate
-    if !signed_in?
-      redirect_to new_session_path
-    end
+    redirect_to new_session_path unless signed_in?
   end
 
   def signed_in?
-    session[:current_email].present?
+    current_email.present?
   end
-  
+
+  def current_email
+    session[:current_email]
+  end
+
+  def sign_in_as(email)
+    session[:current_email] = email
+  end
 end
